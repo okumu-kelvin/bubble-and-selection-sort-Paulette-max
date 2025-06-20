2adef84fcc8381116f757b1622ed02239c6d4eb6
Paulette Mbaika <paulette.mbaika@strathmore.edu>
2025-06-20 11:19:34 +0300

    first test

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,15 @@
 def bubble_sort(unsorted_list):
+    def test_sorted():
     # TODO: Implement bubble sort
+        for n in range(len(unsorted_list) - 1, 0, 1):
+
+            swapped = False
+
+            for i in range(n):
+                if unsorted_list[i] > unsorted_list[i + 1]:
+                    unsorted_list[i], unsorted_list[i + 1] = unsorted_list[i + 1], unsorted_list[i]
+                swapped = True
+            if not swapped:
+                break
+
     pass
--- bubble_sort/test_bubble_sort.py
+++ bubble_sort/test_bubble_sort.py
@@ -1,10 +1,12 @@
+import unittest
 from bubble_sort import bubble_sort
 
-def test_sorted():
-    assert bubble_sort([1, 2, 3]) == [1, 2, 3]
+class testBubbleSort(unittest.TestCase):
+    def test_sorted(self):
+     self.assertEqual(bubble_sort([1, 2, 3]) == [1, 2, 3])
 
-def test_reverse():
-    assert bubble_sort([3, 2, 1]) == [1, 2, 3]
+    def test_reverse(self):
+        self.assertEqual(bubble_sort([3, 2, 1]) == [1, 2, 3])
 
-def test_duplicates():
-    assert bubble_sort([4, 5, 3, 4]) == [3, 4, 4, 5]
+    def test_duplicates(self):
+        self.assertEqual(bubble_sort([4, 5, 3, 4]) == [3, 4, 4, 5])
