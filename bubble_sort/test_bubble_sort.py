b08b09ad9e740ffa7b1bbef52425c79bd058154c
Paulette Mbaika <paulette.mbaika@strathmore.edu>
2025-06-20 11:22:27 +0300

    second test

--- bubble_sort/test_bubble_sort.py
+++ bubble_sort/test_bubble_sort.py
@@ -1,12 +1,10 @@
-import unittest
 from bubble_sort import bubble_sort
 
-class testBubbleSort(unittest.TestCase):
-    def test_sorted(self):
-     self.assertEqual(bubble_sort([1, 2, 3]) == [1, 2, 3])
+def test_sorted():
+    assert bubble_sort([1, 2, 3]) == [1, 2, 3]
 
-    def test_reverse(self):
-        self.assertEqual(bubble_sort([3, 2, 1]) == [1, 2, 3])
+def test_reverse(self):
+    assert bubble_sort([3, 2, 1]) == [1, 2, 3]
 
-    def test_duplicates(self):
-        self.assertEqual(bubble_sort([4, 5, 3, 4]) == [3, 4, 4, 5])
+def test_duplicates(self):
+    assert bubble_sort([4, 5, 3, 4]) == [3, 4, 4, 5]
